6e194716f84554187c8da477c2cd7dfb2d3ee26b
Nitin Mishra <nitin.m@hotmail.com>
2020-09-19 18:27:23 +0530

    Positive scenarios

--- ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart.cs
@@ -21,19 +21,53 @@ namespace ShoppingCart
         {
             if (!_promotions.Any())
             {
-                return CalculatePriceWithoutPromotion();
+                return CalculatePriceWithoutPromotion(CurrentCart);
             }
-            throw new NotImplementedException();
+
+            return CalculatePriceWithPromotions();
         }
 
-        private int CalculatePriceWithoutPromotion()
+        private int CalculatePriceWithoutPromotion(Dictionary<string, int> cart)
         {
             var cartPrice = 0;
             var skuDefaults = _skuPrice.ToDictionary(item => item.SKUId, item => item.Price);
-            foreach (var keyValuePair in CurrentCart)
+            foreach (var keyValuePair in cart)
             {
                 cartPrice += skuDefaults[keyValuePair.Key] * keyValuePair.Value;
             }
+
+            return cartPrice;
+        }
+
+        private int CalculatePriceWithPromotions()
+        {
+            var cartPrice = 0;
+            var skuDefaults = _skuPrice.ToDictionary(item => item.SKUId, item => item.Price);
+            var cartClone = CurrentCart.ToDictionary(item => item.Key, item => item.Value);
+
+            foreach (var promo in _promotions)
+            {
+                var combination = promo.Combination;
+                //If every item in the promo is present in the cart and all their Quantity is >= in the Promo
+                //then only apply otherwise that promotion is not applicable
+                if (combination.All(combination => cartClone.ContainsKey(combination.SKUId) &&
+                     cartClone[combination.SKUId] >= combination.Weigth))
+                {
+                    var price = 0;
+                    foreach (var comb in combination)
+                    {
+                        var promoCount = comb.Weigth;
+                        var cartCount = cartClone[comb.SKUId];
+
+                        price = (cartCount / promoCount) * promo.Price;
+                        cartClone[comb.SKUId] = cartCount % promoCount;
+                    }
+                    cartPrice += price;
+                }
+            }
+
+            //After Promo then whatever is left use it to apply individual logic
+            cartPrice += CalculatePriceWithoutPromotion(cartClone);
 
             return cartPrice;
         }
